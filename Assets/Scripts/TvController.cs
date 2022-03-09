b6c5e46b4f594fa29589a731b9eba843847bbbe7
JaouShingan <lky13587089578@gmail.com>
2022-03-09 15:06:46 -0800

    tv version

---
+++ Assets/Scripts/TvController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+public class TvController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public VideoPlayer videoplayer;
+    public GameObject tvscreen;
+    void Start()
+    {
+        //videoplayer = gameObject.GetComponent<VideoPlayer>();
+        videoplayer.targetMaterialRenderer = tvscreen.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (videoplayer.targetMaterialRenderer == null)
+        //    return;
+        
+    }
+    public void PlayorPauseVideo()
+    {
+        if(videoplayer.isPlaying==true)
+        {
+            videoplayer.Pause();
+        }
+        else 
+        {
+            videoplayer.Play();
+        }
+    }
+}
